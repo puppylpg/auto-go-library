2767ff286b0d8d7f809a2464bf71c9a83ed6733b
puppylpg <shininglhb@163.com>
2022-10-26 01:19:22 +0800

    for xiaowan

--- main.py
+++ main.py
@@ -23,20 +23,60 @@ def read_info():
                 print(name + "添加session失败")
 
 
+all_seat = [
+    {
+        "info": "A201考研自习室 216号",
+        "lib_id": 114224,
+        "seat_key": "33,13",
+        "status": 1
+    },
+    {
+        "info": "A201考研自习室 214号",
+        "lib_id": 114224,
+        "seat_key": "33,15",
+        "status": 1
+    },
+    {
+        "info": "A201考研自习室 222号",
+        "lib_id": 114224,
+        "seat_key": "31,14",
+        "status": 1
+    },
+    {
+        "info": "A201考研自习室 217号",
+        "lib_id": 114224,
+        "seat_key": "33,12",
+        "status": 1
+    },
+    {
+        "info": "A201考研自习室 218号",
+        "lib_id": 114224,
+        "seat_key": "33,11",
+        "status": 1
+    }
+]
+
+
 def go(name: str, s: requests.Session):
-    res = post_home_page(s)
-    often_seat = res['data']['userAuth']['oftenseat']["list"]
+    # res = post_home_page(s)
+    # often_seat = res['data']['userAuth']['oftenseat']["list"]
+    often_seat = all_seat
     often_seat_key = []
     for seat in often_seat:
-        often_seat_key.append([seat['seat_key'], seat['lib_id']])
+        often_seat_key.append([seat['seat_key'], seat['lib_id'], seat['info']])
+    print(often_seat_key)
+
     for key in often_seat_key:
+        print("{}开始预约: {}", name, key)
         res = book(s, key)
         # print(res)
         status = res['data']['userAuth']['reserve']['reserveSeat']
         if status is not None and status:
-            print(name + "预约成功")
+            print("{}预约成功: {}", name, key)
             return
-    print(name + "预约失败")
+        else:
+            print("{}预约失败: {}", name, key)
+    print("{}一个也没约着……", name)
 
 
 def save_info():
@@ -49,13 +89,16 @@ def save_info():
 
 
 def job():
+    print("{}, 开抢！！！", time.ctime(time.time()))
     for i in range(3):
         for user in manager.get_users():
             go(user.name, user.session)
+            time.sleep(1)
 
 
 def job_thread(threadName):
-    schedule.every().day.at("06:00").do(job)
+    schedule.every().day.at("07:00").do(job)
+    schedule.every().day.at("22:35").do(job)
     count_60 = 0
     while True:
         schedule.run_pending()
