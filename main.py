f52285b3f0e4388d87113532cfc19ef8d1b14afd
puppylpg <shininglhb@163.com>
2022-10-26 01:30:10 +0800

    add logger

--- main.py
+++ main.py
@@ -1,11 +1,49 @@
 import json
 import schedule
 import time
+from datetime import datetime
+import os
 from send_request import *
 from manager import Manager
 import argparse
+import logging
 
 manager: Manager = Manager()
+
+DATE_HOUR = str(datetime.now().strftime('%Y-%m-%d-%H'))
+DATE_TIME = DATE_HOUR
+
+# log file
+LOG_PATH = "log"
+NORMAL_LOGGER = os.path.join(LOG_PATH, 'log_' + DATE_TIME + '.log')
+
+formatter = logging.Formatter("%(asctime)s [%(levelname)-5.5s]  %(message)s")
+
+def get_logger(name, log_file, level=logging.DEBUG, format=True):
+    """To setup as many loggers as you want"""
+
+    # output to both console and file
+    console_handler = logging.StreamHandler(sys.stdout)
+    file_handler = logging.FileHandler(log_file, mode='w', encoding='utf-8')
+
+    if format:
+        console_handler.setFormatter(formatter)
+        file_handler.setFormatter(formatter)
+
+    logger = logging.getLogger(name)
+    logger.setLevel(level)
+    logger.addHandler(console_handler)
+    logger.addHandler(file_handler)
+
+    return logger
+
+
+# mkdir if not exist
+if not os.path.exists(os.path.dirname(NORMAL_LOGGER)):
+    os.mkdir(os.path.dirname(NORMAL_LOGGER))
+
+
+log = get_logger('normal', os.path.abspath(NORMAL_LOGGER))
 
 
 def read_info():
@@ -16,11 +54,12 @@ def read_info():
             name = user['name']
             s = get_session(user["cookies"])
             res = post_home_page(s)
+            log.info("post home page res: {}", res)
             if "errors" not in res.keys():
                 manager.add_user(name, user["cookies"], s)
-                print(name + "添加session成功")
+                log.info(name + "添加session成功")
             else:
-                print(name + "添加session失败")
+                log.info(name + "添加session失败")
 
 
 all_seat = [
@@ -59,24 +98,25 @@ all_seat = [
 
 def go(name: str, s: requests.Session):
     # res = post_home_page(s)
+    # log.info("post home page res: {}", res)
     # often_seat = res['data']['userAuth']['oftenseat']["list"]
     often_seat = all_seat
     often_seat_key = []
     for seat in often_seat:
         often_seat_key.append([seat['seat_key'], seat['lib_id'], seat['info']])
-    print(often_seat_key)
+    log.info(often_seat_key)
 
     for key in often_seat_key:
-        print("{}开始预约: {}", name, key)
+        log.info("{}开始预约: {}", name, key)
         res = book(s, key)
-        # print(res)
+        log.info("book result: {}", res)
         status = res['data']['userAuth']['reserve']['reserveSeat']
         if status is not None and status:
-            print("{}预约成功: {}", name, key)
+            log.info("{}预约成功: {}", name, key)
             return
         else:
-            print("{}预约失败: {}", name, key)
-    print("{}一个也没约着……", name)
+            log.info("{}预约失败: {}", name, key)
+    log.info("{}一个也没约着……", name)
 
 
 def save_info():
@@ -89,7 +129,7 @@ def save_info():
 
 
 def job():
-    print("{}, 开抢！！！", time.ctime(time.time()))
+    log.info("{}, 开抢！！！", time.ctime(time.time()))
     for i in range(3):
         for user in manager.get_users():
             go(user.name, user.session)
@@ -108,14 +148,15 @@ def job_thread(threadName):
             for user in manager.get_users():
                 s = user.session
                 res = post_home_page(s)
+                log.info("post home page res: {}", res)
                 if res.get('errors') and res.get('errors')[0].get('code') != 0:
-                    print(res)
-                    print("出现了error")
+                    log.info(res)
+                    log.info("出现了error")
                     exit()
                 hold_validate(s)
             count_60 = 0
             save_info()
-            print("save cookies success")
+            log.info("save cookies success")
 
 
 if __name__ == '__main__':
@@ -123,7 +164,7 @@ if __name__ == '__main__':
     parser.add_argument("-r", "--run-once", type=bool, help="run once to test", default=False)
     read_info()
     if len(manager.get_users()) == 0:
-        print("没有可用的session")
+        log.info("没有可用的session")
         exit()
     if not parser.parse_args().run_once:
         job_thread("job_thread")
